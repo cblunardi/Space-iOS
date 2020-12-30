3e7ac6c685761a64f06093d5765e7592faff7155
Caio Brigagão Lunardi <caio.b.lunardi@gmail.com>
2020-12-30 20:29:23 +0100

    test(Network): improve default mock behaviour
    Signed-off-by: Caio Brigagão Lunardi <caio.b.lunardi@gmail.com>

--- SpaceTests/Mock/Dependencies/HTTPService/HTTPServiceMock.swift
+++ SpaceTests/Mock/Dependencies/HTTPService/HTTPServiceMock.swift
@@ -4,7 +4,7 @@ import XCTest
 
 final class HTTPServiceMock: HTTPServiceProtocol {
     var performBehaviour: (URLRequest) -> AnyPublisher<(data: Data, response: URLResponse), URLError> =
-        { _ in Empty().eraseToAnyPublisher() }
+        { _ in Fail(error: URLError(.notConnectedToInternet)).eraseToAnyPublisher() }
 
     func perform(request: URLRequest) -> AnyPublisher<(data: Data, response: URLResponse), URLError> {
         performBehaviour(request)
